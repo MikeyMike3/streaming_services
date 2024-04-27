cc42b2944f063be55d08380b4fb388a6bb5c6a6b
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-04-26 23:56:36 -0700

    Added a Caracal

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,31 +1,42 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { MovieShowCard } from "../components/MovieShowCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export const Trending = () => {
 	const trending = useLoaderData();
 
 	return (
 		<div className="movie-show-flex">
-			{trending[0].results.map((item) => (
-				<Link
-					to={`${item.media_type}/${item.id.toString()}`}
-					key={item.id}>
-					<MovieShowCard
-						genreIds={item.genre_ids}
-						id={item.id}
-						mediaType={item.media_type}
-						overview={item.overview}
-						posterPath={item.poster_path}
-						backdropPath={item.backdrop_path}
-						releaseDate={item.release_date}
-						voteAverage={item.vote_average}
-						title={item.title}
-						name={item.name}
-						movieGenres={trending[1].genres}
-						showGenres={trending[2].genres}
-					/>
-				</Link>
-			))}
+			<Swiper
+				grabCursor={true}
+				spaceBetween={10}
+				slidesPerView={6.5}
+				direction="horizontal">
+				{trending[0].results.map((item) => (
+					<SwiperSlide key={item.id}>
+						<Link to={`${item.media_type}/${item.id.toString()}`}>
+							<MovieShowCard
+								genreIds={item.genre_ids}
+								id={item.id}
+								mediaType={item.media_type}
+								overview={item.overview}
+								posterPath={item.poster_path}
+								backdropPath={item.backdrop_path}
+								releaseDate={item.release_date}
+								voteAverage={item.vote_average}
+								title={item.title}
+								name={item.name}
+								movieGenres={trending[1].genres}
+								showGenres={trending[2].genres}
+							/>
+						</Link>
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</div>
 	);
 };
