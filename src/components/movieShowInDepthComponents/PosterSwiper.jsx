c5fe152cc677f59377343e8a337ca078f42625ed
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-06-24 16:46:09 -0700

    Update PosterSwiper.jsx

--- src/components/movieShowInDepthComponents/PosterSwiper.jsx
+++ src/components/movieShowInDepthComponents/PosterSwiper.jsx
@@ -8,26 +8,30 @@ import "swiper/css/free-mode";
 export const PosterSwiper = (props) => {
 	return (
 		<div className="poster-container">
-			<Swiper
-				slidesPerView={"auto"}
-				grabCursor={"true"}
-				spaceBetween={10}
-				modules={[FreeMode]}
-				freeMode={{
-					freeMode: true,
-				}}
-				className="mySwiper">
-				{props.array.posters.map((item) => (
-					<SwiperSlide key={item.file_path}>
-						<div
-							className="poster-image"
-							style={{
-								backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.file_path})`,
-							}}
-						/>
-					</SwiperSlide>
-				))}
-			</Swiper>
+			{props.array.posters.length > 0 ? (
+				<Swiper
+					slidesPerView={"auto"}
+					grabCursor={"true"}
+					spaceBetween={10}
+					modules={[FreeMode]}
+					freeMode={{
+						freeMode: true,
+					}}
+					className="mySwiper">
+					{props.array.posters.map((item) => (
+						<SwiperSlide key={item.file_path}>
+							<div
+								className="poster-image"
+								style={{
+									backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.file_path})`,
+								}}
+							/>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			) : (
+				<p>No posters available.</p>
+			)}
 		</div>
 	);
 };
