5eebc9f38a74931bd9bd018249f360622ae41579
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-06-24 16:41:17 -0700

    Updated Credits Component

--- src/components/movieShowInDepthComponents/Credits.jsx
+++ src/components/movieShowInDepthComponents/Credits.jsx
@@ -10,44 +10,53 @@ import { Link } from "react-router-dom";
 import poster from "../../imgs/tmdbPoster.jpg";
 
 export const Credits = (props) => {
+	console.log("props.credits:", props.credits);
+	console.log("Is Array:", Array.isArray(props.credits));
+	console.log("Array Length:", props.credits.length);
 	return (
 		<>
 			<h1 className="heading">Cast</h1>
 			<div className="heading-underline"></div>
 			<div className="cast-card-container">
-				<Swiper
-					grabCursor={true}
-					spaceBetween={0}
-					slidesPerView={"auto"}
-					modules={[FreeMode]}
-					freeMode={{
-						freeMode: true,
-					}}>
-					{props.credits.map((item) => (
-						<SwiperSlide key={item.id}>
-							<Link to={`/person/${item.id.toString()}`}>
-								<div key={item.cast_id} className="cast-card">
-									{item.profile_path === null ? (
-										<img
-											className="cast-card-img"
-											src={poster}
-											alt="movie poster"
-										/>
-									) : (
-										<img
-											className="cast-card-img"
-											src={`https://image.tmdb.org/t/p/w500/${item.profile_path}.jpg`}
-											alt="movie poster"
-										/>
-									)}
-									<div className="cast-info">
-										<p>{item.name}</p>
+				{props.credits.length > 0 ? (
+					<Swiper
+						grabCursor={true}
+						spaceBetween={0}
+						slidesPerView={"auto"}
+						modules={[FreeMode]}
+						freeMode={{
+							freeMode: true,
+						}}>
+						{props.credits.map((item) => (
+							<SwiperSlide key={item.id}>
+								<Link to={`/person/${item.id.toString()}`}>
+									<div
+										key={item.cast_id}
+										className="cast-card">
+										{item.profile_path === null ? (
+											<img
+												className="cast-card-img"
+												src={poster}
+												alt="movie poster"
+											/>
+										) : (
+											<img
+												className="cast-card-img"
+												src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`}
+												alt="movie poster"
+											/>
+										)}
+										<div className="cast-info">
+											<p>{item.name}</p>
+										</div>
 									</div>
-								</div>
-							</Link>
-						</SwiperSlide>
-					))}
-				</Swiper>
+								</Link>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				) : (
+					<p>No cast information available.</p>
+				)}
 			</div>
 		</>
 	);
