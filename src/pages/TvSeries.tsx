c74e095b045626bca488b613df77768492727faa
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-07-08 17:10:43 -0700

    Added Type Safety to TvSeries.tsx

--- src/pages/TvSeries.tsx
+++ src/pages/TvSeries.tsx
@@ -3,9 +3,10 @@ import { useLoaderData } from "react-router-dom";
 import { PopularShows } from "../components/PopularShows";
 import { Hero } from "../components/Hero";
 import { GeneralApiErrorMessage } from "../components/GeneralApiErrorMessage";
+import { HomeLoaderTuple } from "../types/homeTypes";
 
 export const TvSeries = () => {
-	const loaderData = useLoaderData();
+	const loaderData = useLoaderData() as HomeLoaderTuple;
 
 	if (loaderData[6] !== null) {
 		return <GeneralApiErrorMessage />;
